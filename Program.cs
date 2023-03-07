a849e9e927a787b0680f8812e06d98d42de6a19d
sandroXP2022 <73885792+sandroXP2022@users.noreply.github.com>
2023-03-07 19:47:43 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,254 @@
+﻿using OpenAI_API.Completions;
+using OpenAI_API.Models;
+
+var api = new OpenAI_API.OpenAIAPI("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");
+
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("****************************************************************");
+    Console.WriteLine("*                         SNAKE GPT                            *");
+    Console.WriteLine("****************************************************************");
+    Console.WriteLine();
+    Console.WriteLine("Choose an option from the menu: ");
+    Console.WriteLine("\n1 - Ask a Question");
+    Console.WriteLine("2 - Generate Image\n");
+
+    Console.Write("Choose the desired option: ");
+    var option = Console.ReadLine();
+
+    if(option == "1")
+    {
+        Console.Clear();
+        Console.WriteLine("****************************************************************");
+        Console.WriteLine("*                         SNAKE GPT                            *");
+        Console.WriteLine("****************************************************************");
+        Console.WriteLine();
+        Console.WriteLine("Choose model type: \n");
+        Console.WriteLine("1 - Faster");
+        Console.WriteLine("2 - Balanced");
+        Console.WriteLine("3 - More Powerful\n");
+
+        Console.Write("Choose the desired option: ");
+        option = Console.ReadLine();
+
+        if(option == "1") 
+        {
+            Console.Clear();
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("*                         SNAKE GPT                            *");
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine();
+            Console.WriteLine("Choose model style::");
+            Console.WriteLine("\n1 - More creative");
+            Console.WriteLine("2 - Balanced");
+            Console.WriteLine("3 - More Accurate");
+
+            Console.Write("\nChoose the desired option: ");
+            option = Console.ReadLine();
+
+            if( option == "1")
+            {   Console.Clear();
+                Console.WriteLine("****************************************************************");
+                Console.WriteLine("*                         SNAKE GPT                            *");
+                Console.WriteLine("****************************************************************");
+                Console.Clear();
+                Console.Write("Write your question here: ");
+                string request = Console.ReadLine();
+
+                Console.Clear();
+                Console.WriteLine(request);
+                Console.WriteLine("\n");
+
+                var result = await api.Completions.CreateCompletionAsync(new CompletionRequest(request, model: Model.BabbageText, 1000, 0.4));
+                Console.WriteLine(result);
+                Console.ReadKey();
+            }
+            else if(option == "2")
+            {
+                Console.Clear();
+                Console.WriteLine("****************************************************************");
+                Console.WriteLine("*                         SNAKE GPT                            *");
+                Console.WriteLine("****************************************************************");
+                Console.Clear();
+                Console.Write("Write your question here: ");
+                string request = Console.ReadLine();
+
+                Console.Clear();
+                Console.WriteLine(request);
+                Console.WriteLine("\n");
+
+                var result = await api.Completions.CreateCompletionAsync(new CompletionRequest(request, model: Model.BabbageText, 1000, 0.6));
+                Console.WriteLine(result);
+                Console.ReadKey();
+            }
+            else if(option == "3")
+            {
+                Console.Clear();
+                Console.WriteLine("****************************************************************");
+                Console.WriteLine("*                         SNAKE GPT                            *");
+                Console.WriteLine("****************************************************************");
+                Console.Clear();
+                Console.Write("Write your question here: ");
+                string request = Console.ReadLine();
+
+                Console.Clear();
+                Console.WriteLine(request);
+                Console.WriteLine("\n");
+
+                var result = await api.Completions.CreateCompletionAsync(new CompletionRequest(request, model: Model.BabbageText, 1000, 0.8));
+                Console.WriteLine(result);
+                Console.ReadKey();
+            }
+        }
+        else if (option == "2") 
+        {
+            Console.Clear();
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("*                         SNAKE GPT                            *");
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine();
+            Console.WriteLine("Choose model style::");
+            Console.WriteLine("\n1 - More creative");
+            Console.WriteLine("2 - Balanced");
+            Console.WriteLine("3 - 3 - More Accurate");
+
+            Console.Write("\nChoose the desired option: ");
+            option = Console.ReadLine();
+
+            if (option == "1")
+            {
+                Console.Clear();
+                Console.WriteLine("****************************************************************");
+                Console.WriteLine("*                         SNAKE GPT                            *");
+                Console.WriteLine("****************************************************************");
+                Console.Clear();
+                Console.Write("Write your question here: ");
+                string request = Console.ReadLine();
+
+                Console.WriteLine(request);
+                Console.WriteLine("\n");
+
+                var result = await api.Completions.CreateCompletionAsync(new CompletionRequest(request, model: Model.CurieText, 1000, 0.4));
+                Console.WriteLine(result);
+                Console.ReadKey();
+            }
+            else if (option == "2")
+            {
+                Console.Clear();
+                Console.WriteLine("****************************************************************");
+                Console.WriteLine("*                         SNAKE GPT                            *");
+                Console.WriteLine("****************************************************************");
+                Console.Clear();
+                Console.Write("Write your question here: ");
+                string request = Console.ReadLine();
+
+                Console.WriteLine(request);
+                Console.WriteLine("\n");
+
+                var result = await api.Completions.CreateCompletionAsync(new CompletionRequest(request, model: Model.CurieText, 1000, 0.6));
+                Console.WriteLine(result);
+                Console.ReadKey();
+            }
+            else if (option == "3")
+            {
+                Console.Clear();
+                Console.WriteLine("****************************************************************");
+                Console.WriteLine("*                         SNAKE GPT                            *");
+                Console.WriteLine("****************************************************************");
+                Console.Clear();
+                Console.Write("Write your question here: ");
+                string request = Console.ReadLine();
+
+                Console.WriteLine(request);
+                Console.WriteLine("\n");
+
+                var result = await api.Completions.CreateCompletionAsync(new CompletionRequest(request, model: Model.CurieText, 1000, 0.8));
+                Console.WriteLine(result);
+                Console.ReadKey();
+            }
+        }
+    
+    if(option == "3")
+        {
+            Console.Clear();
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("*                         SNAKE GPT                            *");
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine();
+            Console.WriteLine("Choose model style::");
+            Console.WriteLine("\n1 - More creative");
+            Console.WriteLine("2 - Balanced");
+            Console.WriteLine("3 - 3 - More Accurate");
+
+            Console.Write("\nChoose the desired option: ");
+            option = Console.ReadLine();
+
+            if (option == "1")
+            {
+                Console.Clear();
+                Console.WriteLine("****************************************************************");
+                Console.WriteLine("*                         SNAKE GPT                            *");
+                Console.WriteLine("****************************************************************");
+                Console.Clear();
+                Console.Write("Write your question here: ");
+                string request = Console.ReadLine();
+
+                Console.WriteLine(request);
+                Console.WriteLine("\n");
+
+                var result = await api.Completions.CreateCompletionAsync(new CompletionRequest(request, model: Model.DavinciText, 1000, 0.4));
+                Console.WriteLine(result);
+                Console.ReadKey();
+            }
+            else if (option == "2")
+            {
+                Console.Clear();
+                Console.WriteLine("****************************************************************");
+                Console.WriteLine("*                         SNAKE GPT                            *");
+                Console.WriteLine("****************************************************************");
+                Console.Clear();
+                Console.Write("Write your question here: ");
+                string request = Console.ReadLine();
+
+                Console.WriteLine(request);
+                Console.WriteLine("\n");
+
+                var result = await api.Completions.CreateCompletionAsync(new CompletionRequest(request, model: Model.DavinciText, 1000, 0.6));
+                Console.WriteLine(result);
+                Console.ReadKey();
+            }
+            else if (option == "3")
+            {
+                Console.Clear();
+                Console.WriteLine("****************************************************************");
+                Console.WriteLine("*                         SNAKE GPT                            *");
+                Console.WriteLine("****************************************************************");
+                Console.Clear();
+                Console.Write("Write your question here: ");
+                string request = Console.ReadLine();
+
+                Console.WriteLine(request);
+                Console.WriteLine("\n");
+
+                var result = await api.Completions.CreateCompletionAsync(new CompletionRequest(request, model: Model.DavinciText, 1000, 0.8));
+                Console.WriteLine(result);
+                Console.ReadKey();
+            }
+        }  
+    
+    }
+    if(option == "2")
+    {
+        Console.Clear();
+        Console.WriteLine("Unavalaible!!!");
+        Console.ReadKey();
+    }
+
+
+
+
+
+
+}
